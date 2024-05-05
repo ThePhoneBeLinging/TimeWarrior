01f6d507d1d4455f8304f6eb5a059ff087c4a6ba
Elias Larsen <s235125@dtu.dk>
2024-05-05 18:43:22 +0200

    Initial Commit

---
+++ Main.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Elias on 05/05/2024.
+//
+
+#include "Main.h"
+#include "raylib.h"
+#include <iostream>
+int main()
+{
+    std::cout << "Hello world";
+    return 0;
+}
---
+++ Main.h
@@ -0,0 +1,15 @@
+//
+// Created by Elias on 05/05/2024.
+//
+
+#ifndef TIMEWARRIOR_MAIN_H
+#define TIMEWARRIOR_MAIN_H
+
+
+class Main
+{
+
+};
+
+
+#endif //TIMEWARRIOR_MAIN_H
