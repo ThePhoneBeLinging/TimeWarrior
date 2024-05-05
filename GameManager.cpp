830c91ace51d3c5df91345059ee3d9f446223b09
Elias Larsen <s235125@dtu.dk>
2024-05-05 19:33:20 +0200

    Refactoring

--- GameManager.cpp
+++ GameManager.cpp
@@ -5,6 +5,16 @@
 #include <iostream>
 #include "GameManager.h"
 
+GameManager::GameManager (int screenWidth, int screenHeight, int fps)
+{
+    this->player = new Player(0,0,50,50);
+    this->screenWidth = screenWidth;
+    this->screenHeight = screenHeight;
+    InitWindow(screenWidth,screenHeight,"Time - Warrior");
+    SetTargetFPS(fps);
+
+}
+
 void GameManager::gameLoop()
 {
     while (!WindowShouldClose())
@@ -16,16 +26,6 @@ void GameManager::gameLoop()
         DrawText("Hello World",250,250,15,WHITE);
         EndDrawing();
     }
-
-}
-
-GameManager::GameManager (int screenWidth, int screenHeight, int fps)
-{
-    this->player = new Player(0,0,50,50);
-    this->screenWidth = screenWidth;
-    this->screenHeight = screenHeight;
-    InitWindow(screenWidth,screenHeight,"Time - Warrior");
-    SetTargetFPS(fps);
 
 }
 
