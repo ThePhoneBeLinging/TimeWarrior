72a76aa740f554ef4a096ad243cc67550a424c37
Elias Larsen <s235125@dtu.dk>
2024-05-05 18:49:07 +0200

    Added window

--- Main.cpp
+++ Main.cpp
@@ -4,8 +4,27 @@
 
 #include "raylib.h"
 #include <iostream>
+void gameLoop();
 int main()
 {
+    int screenHeight = 800;
+    int screenWidth = 1200;
+    InitWindow(screenWidth,screenHeight,"Time - Warrior");
+    SetTargetFPS(60);
+    gameLoop();
     std::cout << "Hello world";
+    CloseWindow();
     return 0;
+}
+
+void gameLoop()
+{
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+        ClearBackground(BLACK);
+        DrawText("Hello World",250,250,15,WHITE);
+        EndDrawing();
+    }
+
 }
