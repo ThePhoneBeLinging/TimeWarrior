a38a8ebacade2a7fc1ae278e0aad325bd7a3ad70
Elias Larsen <s235125@dtu.dk>
2024-05-14 20:10:20 +0200

    PressurePlate changes texture on activation

--- PressurePlate.cpp
+++ PressurePlate.cpp
@@ -9,15 +9,17 @@ PressurePlate::PressurePlate (int x, int y, int width, int height):
 {
     this->idleTexture = LoadTexture("../Resources/wood.png");
     this->activatedTexture = LoadTexture("../Resources/zelda.png");
-    this->activated = false;
+    this->deactivate();
 }
 
 void PressurePlate::activate ()
 {
     this->activated = true;
+    this->setTexture(this->activatedTexture);
 }
 
 void PressurePlate::deactivate ()
 {
     this->activated = false;
+    this->setTexture(this->idleTexture);
 }
